6eab70c1b4218efdbc304fd468d5c0cc281b19b1
Ikilemg <cevik190506@gmail.com>
2022-06-25 02:13:27 +0300

    init

---
+++ lib/main.dart
@@ -0,0 +1,84 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter anan',
+      theme: ThemeData(
+        primarySwatch: Colors.pink,
+      ),
+      home: const MyHomePage(title: 'Calculator'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  Widget buildButton() {
+    return Expanded(
+      child: OutlinedButton(
+        onPressed: () => {},
+        child: const Text("1"),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(widget.title),
+        ),
+        body: Column(
+          children: <Widget>[
+            const Text("0"),
+            const Expanded(
+              child: Divider(),
+            ),
+            Column(
+              children: [
+                Row(children: [
+                  buildButton(),
+                  buildButton(),
+                  buildButton(),
+                  buildButton(),
+                ]),
+                Row(children: [
+                  buildButton(),
+                  buildButton(),
+                  buildButton(),
+                  buildButton(),
+                ]),
+                Row(children: [
+                  buildButton(),
+                  buildButton(),
+                  buildButton(),
+                  buildButton(),
+                ]),
+                Row(children: [
+                  buildButton(),
+                  buildButton(),
+                  buildButton(),
+                  buildButton(),
+                ]),
+              ],
+            )
+          ],
+        ));
+  }
+}
